c151795f3c4b5b26658a583e16f088a7776c9dd6
Alexander Kraus <nr4@z10.info>
2022-01-28 23:04:37 +0100

    added window main loop

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,22 @@
 #include <SFML/Window.hpp>
 
-int main(int argc, char **args)
+#include <stdio.h>
+
+int main()
 {
+    printf("Hi.");
     sf::Window window(sf::VideoMode(800, 600), "My window");
-    
+    printf("Created window.");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
     return 0;
 }
-
