45e507379fd9e6dda89d652b75271b1c9891a1bb
Alexander Kraus <nr4@z10.info>
2022-01-29 21:03:07 +0100

    added highscore list

---
+++ model/HighscoreList.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+class HighscoreListEntry {
+    int _score;
+    std::string _name;
+
+    public:
+    HighscoreListEntry(std::string name, int score);
+    static bool compare(HighscoreListEntry a, HighscoreListEntry b);
+    int score();
+    std::string name();
+};
+
+class HighscoreList {
+    std::vector<HighscoreListEntry> _entries;
+
+    public:
+    HighscoreList();
+    void add(std::string name, int score);
+
+    static const std::string highscoreFileName;
+    static const int nEntries;
+};
