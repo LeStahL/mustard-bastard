16bd9dcf5e82d1d2b8f0d8f792555285b163e93d
Max Zimmermann <mz.max.zimmermann@gmail.com>
2022-01-30 17:58:42 +0100

    Enemies walking

--- model/Model.cpp
+++ model/Model.cpp
@@ -7,7 +7,8 @@ const WorldPosition START_PLAYER1 = WorldPosition(40., 1, true);
 
 Model::Model() {
     players.push_back(Player(GraphicsId::player_standing, START_PLAYER1));
-    enemies.push_back(Enemy(EnemyType::IcebergAndFairy, WorldPosition(500.0f, 1, true))); 
+    //enemies.push_back(Enemy(EnemyType::IcebergAndFairy, WorldPosition(500.0f, 1, true)));
+    //enemies.push_back(Enemy(EnemyType::ZombieAndCat, WorldPosition(20.0f, 0, true)));  
 }
 Player* Model::getPlayer(int player_number) {
     return &players[player_number];
