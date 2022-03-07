ed42f9272a3553450bbdb338098f8a043b0d48db
Max Zimmermann <mz.max.zimmermann@gmail.com>
2022-03-07 22:02:39 +0100

    cooldown class

---
+++ utility/Cooldown.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+class Cooldown {
+private:
+    float cooldown;
+
+public:
+    Cooldown(float cooldown) : cooldown(cooldown) {}
+    Cooldown() : Cooldown(0.0f) {}
+
+    float getCooldown() const { return cooldown; }
+    void setCooldown(const float cooldown) { this->cooldown = cooldown > 0.0f ? cooldown : 0.0f; }
+    void coolDownBy(const float elapsedTime) { setCooldown(cooldown - elapsedTime); }
+};
